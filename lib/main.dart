54a8ec8f4f3d6f8afa2c0426aae5f194a4090fb7
SYeameen <sajjadyeameen@gmail.com>
2025-01-25 18:28:16 +0600

    finalized commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,21 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(),
-    ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue[300],
+        appBar: AppBar(
+          centerTitle: true,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+          backgroundColor: Colors.teal[900],
+          title: Text('Hi! This is Coal'),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('Imgs/coal.png'),
+          ),
+        ), //AppBar
+      ), //Scaffold
+    ), //Material App
   );
 }
